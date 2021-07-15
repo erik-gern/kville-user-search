b2f376bde7e3ac4276c6547e39d82866a63d0011
Erik Gern <erik.gern@gmail.com>
2021-07-15 12:32:59 -0400

    added browserify build

---
+++ src/browser.jsx
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const AppManager = require('./components/AppManager');
+
+ReactDOM.render(
+	<AppManager />,
+	document.getElementById('root')
+);
